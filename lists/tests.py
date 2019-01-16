5c0a4ccd9fda410cafa6bce36583de40c68a8d05
anzi <132354@qq.com>
2019-01-16 15:15:14 +0800

    重构主页视图以使用模板 使用Django 测试客户端 4.3 牢记不要测试常量，应该测试实现方式- 单元测试规则

--- lists/tests.py
+++ lists/tests.py
@@ -4,21 +4,12 @@ from django.test import TestCase
 from  lists.views import  home_page
 from  django.http import  HttpRequest
 
-
 class HomePageTest(TestCase):
-#这个方法是检查“/” 能否映射到 home_page 函数
-    def test_url_resolve_to_home_page_view(self):
-        found = resolve("/")
-        # resolve 用于解析URl ,并映射到相应的视图函数。
-        self.assertEqual(found.func, home_page)
+    def test_uses_home_template(self):
+        response= self.client.get('/')
+        self.assertTemplateUsed(response,"home.html")
 
-#视图函数的测试
-    def test_home_page_returns_correct_html(self):
-        request = HttpRequest()
-        response = home_page(request)
-        html = response.content.decode('utf-8')
-        self.assertTrue(html.strip().startswith('<html>'))
-        self.assertIn("<title>To-Do list</title>",html)
-        self.assertTrue(html.endswith('</html>'))
+
+
 
 
