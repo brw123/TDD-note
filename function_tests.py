15cc866648bd0edfe5baa8eeaf2f1b9d495ee679
anzi <132354@qq.com>
2019-01-18 23:07:38 +0800

    重构优化功能测试5.4

--- function_tests.py
+++ function_tests.py
@@ -10,7 +10,12 @@ class NewVisitorTest(unittest.TestCase):
         self.browser = webdriver.Firefox()
     def tearDown(self):
         self.browser.quit()
-
+    def check_for_row_in_list_table(self,row_text):
+        table = self.browser.find_element_by_id("id_list_table")
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(
+            row_text ,[row.text for row in rows]
+        )
     def test_can_start_a_list_and_retrieve_it_later(self):
 #小花听说有一个待办事项的网站很好
 #她去看了这个应用的首页
@@ -33,13 +38,7 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
 # 待办事项中显示“1：Buy woolen yarn”
-        table = self.browser.find_element_by_id('id_list_table')
-        rows  = table.find_elements_by_tag_name('tr')
-        self.assertIn(
-          "1:Buy woolen yarn",[row.text for row in rows])
-
-
-
+        self.check_for_row_in_list_table( "1:Buy woolen yarn")
 #页面中又显示了一个文本框，可以输入其他待办事项
 #她输入了"Knit a sweater with wool”(使用毛线织毛衣)
         inputbox = self.browser.find_element_by_id('id_new_item')
@@ -49,12 +48,8 @@ class NewVisitorTest(unittest.TestCase):
 #页面再次跟新，他的清单中显示了这两个待办事项
 # 待办事项中显示  1：Buy woolen yarn
 #                 2:Knit a sweater with wool
-        table = self.browser.find_element_by_id('id_list_table')
-        rows  = table.find_elements_by_tag_name('tr')
-        self.assertIn(
-          "1:Buy woolen yarn",[row.text for row in rows])
-        self.assertIn(
-          "2:Knit a sweater with wool",[row.text for row in rows])
+        self.check_for_row_in_list_table( "1:Buy woolen yarn")
+        self.check_for_row_in_list_table( "2:Buy woolen yarn")
         self.fail("Finish the test")
 
 #[..]
