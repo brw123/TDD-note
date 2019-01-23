7076c266781fddfba54904cdeed48a758cd44302
anzi <132354@qq.com>
2019-01-23 14:15:09 +0800

    去掉功能测试中含糊的等待，改为显示等待

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -3,6 +3,8 @@
 from selenium import webdriver
 from django.test import LiveServerTestCase
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import WebDriverException
+MAX_WAIT = 10
 import time
 class NewVisitorTest(LiveServerTestCase):
 
@@ -10,12 +12,20 @@ class NewVisitorTest(LiveServerTestCase):
         self.browser = webdriver.Firefox()
     def tearDown(self):
         self.browser.quit()
-    def check_for_row_in_list_table(self,row_text):
-        table = self.browser.find_element_by_id("id_list_table")
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn(
-            row_text ,[row.text for row in rows]
-        )
+    def wait_for_row_in_list_table(self,row_text):
+        start_time = time.time()
+        while True:
+            try:
+                table = self.browser.find_element_by_id("id_list_table")
+                rows = table.find_elements_by_tag_name('tr')
+                self.assertIn(
+                    row_text ,[row.text for row in rows]
+                )
+                return
+            except(AssertionError,WebDriverException) as e:
+                if time.time()-start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
     def test_can_start_a_list_and_retrieve_it_later(self):
 #小花听说有一个待办事项的网站很好
 #她去看了这个应用的首页
@@ -32,24 +42,21 @@ class NewVisitorTest(LiveServerTestCase):
         )
 #她在文本框输入了"Buy  woolen yarn " (购买毛线)
 #她的爱好是织毛衣
-        inputbox.send_keys("Buy woolen yarn")
-
 #按回车键页面更新了
+        inputbox.send_keys("Buy woolen yarn")
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
 # 待办事项中显示“1：Buy woolen yarn”
-        self.check_for_row_in_list_table( "1:Buy woolen yarn")
+        self.wait_for_row_in_list_table( "1:Buy woolen yarn")
 #页面中又显示了一个文本框，可以输入其他待办事项
 #她输入了"Knit a sweater with wool”(使用毛线织m毛衣)
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys("Knit a sweater with wool")
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
 #页面再次跟新，他的清单中显示了这两个待办事项
 # 待办事项中显示  1：Buy woolen yarn
 #                 2:Knit a sweater with wool
-        self.check_for_row_in_list_table( "1:Buy woolen yarn")
-        self.check_for_row_in_list_table( "2:Knit a sweater with wool")
+        self.wait_for_row_in_list_table( "1:Buy woolen yarn")
+        self.wait_for_row_in_list_table( "2:Knit a sweater with wool")
         self.fail("Finish the test")
 
 #[..]
